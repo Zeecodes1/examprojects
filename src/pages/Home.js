dc9c787dd6c13aa52935de7eaa3caf287039d8f2
SHOBOWALE ABDUL-AZEEZ <sobowaleolayiwola@gmail.com>
2022-11-04 21:27:22 +0800

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home--container">
-      <h1>Welcome</h1>
+      <h1>Welcome to Counter Page</h1>
       <p><Link to="/counter">Counter Page</Link></p>
     </div>
   );
