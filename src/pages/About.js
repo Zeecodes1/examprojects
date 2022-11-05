4db1e6e06fcfc045039bc8b5e036e9784653309f
SHOBOWALE ABDUL-AZEEZ <sobowaleolayiwola@gmail.com>
2022-11-05 10:50:44 +0800

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <div className="about--container">
-        <h1>About</h1>
+        <h1>ABOUT</h1>
         <p>
           This app is created to be able to increase, decrease, reset and set values with the help of a useReducer that implements a counter
         </p>
